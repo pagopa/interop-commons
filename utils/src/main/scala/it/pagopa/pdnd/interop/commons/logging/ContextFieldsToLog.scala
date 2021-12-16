ce8819b7e529e6a56d6211ef7ba976cb868db280
P3trur0 <ubaldopescatore@gmail.com>
2021-12-16 18:16:40 +0100

    removing context fields

--- utils/src/main/scala/it/pagopa/pdnd/interop/commons/logging/ContextFieldsToLog.scala
+++
@@ -1,3 +0,0 @@
-package it.pagopa.pdnd.interop.commons.logging
-
-final case class ContextFieldsToLog(correlationId: Option[String] = None, userId: Option[String] = None)
--- utils/src/main/scala/it/pagopa/pdnd/interop/commons/logging/logging.scala
+++ utils/src/main/scala/it/pagopa/pdnd/interop/commons/logging/logging.scala
@@ -1,8 +1,10 @@
 package it.pagopa.pdnd.interop.commons
 
 import com.typesafe.scalalogging.CanLog
+import it.pagopa.pdnd.interop.commons.utils.{CORRELATION_ID_HEADER, UID}
 
 package object logging {
+  type ContextFieldsToLog = Seq[(String, String)]
 
   /** Defines log message decoration for PDND
     */
@@ -10,7 +12,8 @@ package object logging {
     @inline private def optToLog(opt: Option[String]): String = opt.getOrElse("")
 
     override def logMessage(originalMsg: String, fields: ContextFieldsToLog): String = {
-      s"[${optToLog(fields.userId)}] [${optToLog(fields.correlationId)}] - $originalMsg"
+      val fieldsMap = fields.toMap
+      s"[${optToLog(fieldsMap.get(UID))}] [${optToLog(fieldsMap.get(CORRELATION_ID_HEADER))}] - $originalMsg"
     }
   }
 }
