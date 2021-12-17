2721d6a351d18a4868943670483b4f67f8ae4886
P3trur0 <ubaldopescatore@gmail.com>
2021-12-17 11:09:44 +0100

    improving logging

--- jwt/src/main/scala/it/pagopa/pdnd/interop/commons/jwt/service/impl/DefaultJWTReader.scala
+++ jwt/src/main/scala/it/pagopa/pdnd/interop/commons/jwt/service/impl/DefaultJWTReader.scala
@@ -3,7 +3,6 @@ package it.pagopa.pdnd.interop.commons.jwt.service.impl
 import com.nimbusds.jwt.{JWTClaimsSet, SignedJWT}
 import it.pagopa.pdnd.interop.commons.jwt.PublicKeysHolder
 import it.pagopa.pdnd.interop.commons.jwt.service.JWTReader
-import org.slf4j.{Logger, LoggerFactory}
 
 import scala.util.Try
 
@@ -12,8 +11,6 @@ import scala.util.Try
 trait DefaultJWTReader extends JWTReader {
 
   publicKeysHolder: PublicKeysHolder =>
-
-  private val logger: Logger = LoggerFactory.getLogger(this.getClass)
 
   override def getClaims(bearer: String): Try[JWTClaimsSet] = {
     for {
