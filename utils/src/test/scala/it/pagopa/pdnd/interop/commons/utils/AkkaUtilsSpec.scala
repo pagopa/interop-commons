9ba874d4aa8fbf586f138f9209fd675559ebbf85
Antonio Gelameris <toniogela89@gmail.com>
2022-02-01 16:57:37 +0100

    Adding Uid extractor

--- utils/src/test/scala/it/pagopa/pdnd/interop/commons/utils/AkkaUtilsSpec.scala
+++ utils/src/test/scala/it/pagopa/pdnd/interop/commons/utils/AkkaUtilsSpec.scala
@@ -6,8 +6,16 @@ import akka.http.scaladsl.server.directives.Credentials.Missing
 import it.pagopa.pdnd.interop.commons.utils.AkkaUtils.{Authenticator, PassThroughAuthenticator}
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpecLike
+import it.pagopa.pdnd.interop.commons.utils.{BEARER, UID}
+import it.pagopa.pdnd.interop.commons.utils.AkkaUtils._
+import scala.concurrent.duration._
+import scala.util.{Failure, Success}
+import scala.concurrent.Future
+import it.pagopa.pdnd.interop.commons.utils.errors.GenericComponentErrors
+import scala.concurrent.Await
+import org.scalatest.concurrent.ScalaFutures
 
-class AkkaUtilsSpec extends AnyWordSpecLike with Matchers {
+class AkkaUtilsSpec extends AnyWordSpecLike with Matchers with ScalaFutures {
 
   "an Authenticator" should {
     "retrieve the bearer token from the input credentials" in {
@@ -32,6 +40,33 @@ class AkkaUtilsSpec extends AnyWordSpecLike with Matchers {
     "retrieve an empty sequence of contexts when no credentials are set" in {
       val credentials: Credentials = Missing
       PassThroughAuthenticator.apply(credentials) shouldBe Some(Seq.empty)
+    }
+  }
+
+  "a Context" should {
+    "return an uid if contained" in {
+      val contexts: Seq[(String, String)] = List((UID, "doone"))
+      getUid(contexts) shouldBe Success("doone")
+      getUidFuture(contexts).futureValue shouldBe "doone"
+    }
+
+    "return a bearer if contained" in {
+      val contexts: Seq[(String, String)] = List((BEARER, "RoarerIAmABearer"))
+      getBearer(contexts) shouldBe Success("RoarerIAmABearer")
+      getFutureBearer(contexts).futureValue shouldBe "RoarerIAmABearer"
+    }
+
+    "return a 9996 error if doesn't contain uid" in {
+      val contexts: Seq[(String, String)] = List((BEARER, "doone"))
+      getUid(contexts) shouldBe Failure(GenericComponentErrors.MissingUid)
+      getUidFuture(contexts).failed.futureValue shouldBe GenericComponentErrors.MissingUid
+
+    }
+
+    "return a 9999 error if doesn't contain bearer" in {
+      val contexts: Seq[(String, String)] = List((UID, "WhereIsYoghi?"))
+      getBearer(contexts) shouldBe Failure(GenericComponentErrors.MissingBearer)
+      getFutureBearer(contexts).failed.futureValue shouldBe GenericComponentErrors.MissingBearer
     }
   }
 
