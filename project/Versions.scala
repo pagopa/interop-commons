943b4a0c16dc2fb82786e799c0b612888068861c
Scala Steward <scala-steward@pagopa.it>
2022-03-16 15:22:40 +0000

    Update akka-actor-typed, akka-slf4j, ... to 2.6.18 in 0.1.x

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,7 +1,7 @@
 object Versions {
   lazy val akkaHttpJson4sVersion      = "1.38.2"
   lazy val akkaHttpVersion            = "10.2.6"
-  lazy val akkaVersion                = "2.6.17"
+  lazy val akkaVersion                = "2.6.18"
   lazy val apacheCommonsTextVersion   = "1.9"
   lazy val awsSdkVersion              = "2.17.72"
   lazy val azureStorageBlobVersion    = "12.14.2"
