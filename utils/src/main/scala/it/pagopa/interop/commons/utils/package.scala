05cea34a1f6531bf6ebe872ee25bf6eb2f6ac77d
P3trur0 <ubaldopescatore@gmail.com>
2022-03-23 16:29:45 +0100

    add extractHeaders def

--- utils/src/main/scala/it/pagopa/interop/commons/utils/package.scala
+++ utils/src/main/scala/it/pagopa/interop/commons/utils/package.scala
@@ -1,7 +1,11 @@
 package it.pagopa.interop.commons
 
+import it.pagopa.interop.commons.utils.errors.ComponentError
+import it.pagopa.interop.commons.utils.errors.GenericComponentErrors.{MissingBearer, MissingHeader}
+
 import java.security.MessageDigest
 import java.time.format.DateTimeFormatter
+import scala.concurrent.ExecutionContext
 
 package object utils {
   private[utils] lazy val dateFormatter: DateTimeFormatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME
@@ -13,4 +17,15 @@ package object utils {
   val CORRELATION_ID_HEADER: String                        = "X-Correlation-Id"
   val IP_ADDRESS: String                                   = "X-Forwarded-For"
   val INTEROP_PRODUCT_NAME: String                         = "interop"
+
+  def extractHeaders(
+    contexts: Seq[(String, String)]
+  )(implicit ec: ExecutionContext): Either[ComponentError, (String, String, Option[String])] = {
+    val contextsMap = contexts.toMap
+    for {
+      bearerToken   <- contextsMap.get(BEARER).toRight(MissingBearer)
+      correlationId <- contextsMap.get(CORRELATION_ID_HEADER).toRight(MissingHeader(CORRELATION_ID_HEADER))
+      ip = contextsMap.get(IP_ADDRESS)
+    } yield (bearerToken, correlationId, ip)
+  }
 }
