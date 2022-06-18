8a5d9ef85a02ca66e38f6a6509be6af265bfb83a
Scala Steward <scala-steward@pagopa.it>
2022-06-18 06:55:41 +0000

    Update cats-core to 2.8.0 in 0.1.x

--- project/Versions.scala
+++ project/Versions.scala
@@ -4,7 +4,7 @@ object Versions {
   lazy val akkaVersion                = "2.6.17"
   lazy val apacheCommonsTextVersion   = "1.9"
   lazy val azureStorageBlobVersion    = "12.15.0"
-  lazy val catsVersion                = "2.7.0"
+  lazy val catsVersion                = "2.8.0"
   lazy val commonsFileUploadVersion   = "1.4"
   lazy val courierVersion             = "3.1.0"
   lazy val httpJson4sVersion          = "1.38.2"
