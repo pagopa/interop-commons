67a939ad187788021b0008f3ee102ebb40f419c1
Antonio Gelameris <toniogela89@gmail.com>
2022-06-24 15:59:27 +0200

    Reafactring queue handler and exposing raw process

--- queue-manager/src/main/scala/it/pagopa/interop/commons/queue/impl/SQSHandler.scala
+++ queue-manager/src/main/scala/it/pagopa/interop/commons/queue/impl/SQSHandler.scala
@@ -36,34 +36,46 @@ final case class SQSHandler(queueUrl: String)(implicit ec: ExecutionContext) {
     sqsClient.deleteMessage(deleteMessageRequest)
   }.void
 
-  def processMessages[T: JsonReader, V](maxNumberOfMessages: Int, visibilityTimeout: Int)(
-    fn: T => Future[V]
-  ): Future[List[(V, String)]] = for {
-    messagesAndHandles <- rawReceiveNBodyAndHandle(maxNumberOfMessages, visibilityTimeout)
-    result             <- Future.traverse(messagesAndHandles) { case (m, h) => toJson(m).flatMap(fn).map((_, h)) }
-  } yield result
+  /** Fetches 10 messages at time until it reaches `chunkSize` and gives you back 
+    * both the raw messages and the handles. Runs until the queue is empty*/
+  def processAllRawMessages(chunkSize: Int, visibilityTimeout: Int)(
+    fn: (List[String], List[String]) => Future[Unit]
+  ): Future[Unit] = loop(rawReceiveNBodyAndHandle(10, visibilityTimeout))(chunkSize, Nil)(fn)
 
   /** Fetches 10 messages at time until it reaches `chunkSize` and gives you back 
     * both the deserialized messages and the handles. Runs until the queue is empty*/
   def processAllMessages[T: JsonReader](chunkSize: Int, visibilityTimeout: Int)(
     fn: (List[T], List[String]) => Future[Unit]
-  ): Future[Unit] = {
+  ): Future[Unit] = loop(processMessages[T, T](10, visibilityTimeout)(Future.successful))(chunkSize, Nil)(fn)
 
-    def accumulateMessages(acc: List[(T, String)]): Future[List[(T, String)]] =
-      processMessages[T, T](10, visibilityTimeout)(Future.successful).flatMap { messages =>
-        val chunk: List[(T, String)] = acc ++ messages
-        if (messages.isEmpty) Future.successful(acc)
-        else if (chunk.size >= chunkSize) Future.successful(chunk)
-        else accumulateMessages(chunk)
-      }
+  private def loop[T](getMessages: => Future[List[(T, String)]])(chunkSize: Int, acc: List[(T, String)])(
+    fn: (List[T], List[String]) => Future[Unit]
+  ) = innerLoop(accumulateMessages(getMessages)(chunkSize, Nil))(fn)
 
-    def loop: Future[Unit] = accumulateMessages(Nil).flatMap { messagesAndHandles =>
+  private def innerLoop[T](
+    accumulatedMessages: => Future[List[(T, String)]]
+  )(fn: (List[T], List[String]) => Future[Unit]): Future[Unit] =
+    accumulatedMessages.flatMap { messagesAndHandles =>
       if (messagesAndHandles.isEmpty) Future.unit
-      else fn.tupled(messagesAndHandles.unzip[T, String]).flatMap(_ => loop)
+      else fn.tupled(messagesAndHandles.unzip[T, String]).flatMap(_ => innerLoop[T](accumulatedMessages)(fn))
     }
 
-    loop
-  }
+  private def accumulateMessages[T](
+    getMessages: => Future[List[(T, String)]]
+  )(chunkSize: Int, acc: List[(T, String)]): Future[List[(T, String)]] =
+    getMessages.flatMap { messages =>
+      val chunk: List[(T, String)] = acc ++ messages
+      if (messages.isEmpty) Future.successful(acc)
+      else if (chunk.size >= chunkSize) Future.successful(chunk)
+      else accumulateMessages(getMessages)(chunkSize, chunk)
+    }
+
+  private def processMessages[T: JsonReader, V](maxNumberOfMessages: Int, visibilityTimeout: Int)(
+    fn: T => Future[V]
+  ): Future[List[(V, String)]] = for {
+    messagesAndHandles <- rawReceiveNBodyAndHandle(maxNumberOfMessages, visibilityTimeout)
+    result             <- Future.traverse(messagesAndHandles) { case (m, h) => toJson(m).flatMap(fn).map((_, h)) }
+  } yield result
 
   private def toJson[T: JsonReader](s: String): Future[T] = Try(s.parseJson) match {
     case Failure(_)    => Future.failed(DeserializationException(s"Not a valid json: $s"))
