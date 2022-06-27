a21005ec56874b84b9293ce3478297093b5d8ea8
galales <25105748+galales@users.noreply.github.com>
2022-06-27 16:54:52 +0200

    pin-1178: Minor comment

--- file-manager/src/main/scala/it/pagopa/interop/commons/files/model/PDFConfiguration.scala
+++ file-manager/src/main/scala/it/pagopa/interop/commons/files/model/PDFConfiguration.scala
@@ -1,6 +1,10 @@
 package it.pagopa.interop.commons.files.model
 
 // Url must begin and end with the separator "/"
+
+/**
+  * @param resourcesBaseUrl base path for assets references used inside html template
+  */
 final case class PDFConfiguration(resourcesBaseUrl: Option[String])
 
 object PDFConfiguration {
