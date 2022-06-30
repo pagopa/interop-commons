1028ba6bf312ec53ecc47398e3e82cac8c46f2d3
Antonio Gelameris <toniogela89@gmail.com>
2022-06-30 12:22:25 +0200

    Removed useless class

--- file-manager/src/main/scala/it/pagopa/interop/commons/files/StorageConfiguration.scala
+++ file-manager/src/main/scala/it/pagopa/interop/commons/files/StorageConfiguration.scala
@@ -1,8 +1,6 @@
 package it.pagopa.interop.commons.files
 
-import com.typesafe.config.{Config, ConfigFactory}
-
-case class StorageAccountInfo(applicationId: String, applicationSecret: String, endpoint: String)
+import com.typesafe.config.ConfigFactory
 
 object StorageConfiguration {
   val maxConcurrency: Int = ConfigFactory.load().getInt("interop-commons.storage.max-concurrency")
