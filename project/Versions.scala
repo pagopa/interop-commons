8bf887af5df9702cd103a892dfd0c138beb10982
Scala Steward <scala-steward@pagopa.it>
2022-10-05 15:18:46 +0000

    Update logback-classic to 1.4.3 in 1.0.x

--- project/Versions.scala
+++ project/Versions.scala
@@ -14,7 +14,7 @@ object Versions {
   lazy val json4sVersion              = "4.0.4"
   lazy val jsoupVersion               = "1.15.2"
   lazy val lightbendLoggingVersion    = "3.9.5"
-  lazy val logbackVersion             = "1.2.11"
+  lazy val logbackVersion             = "1.4.3"
   lazy val mockJavaMailVersion        = "1.9"
   lazy val mongodbScalaDriverVersion  = "4.6.0"
   lazy val nimbusVersion              = "9.23"
