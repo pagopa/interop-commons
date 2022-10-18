6c6cce18fc574c1722437e1cf60262dcb1c53d76
galales <25105748+galales@users.noreply.github.com>
2022-10-18 12:29:01 +0200

    pin-2063: Add content md5 on S3 put

--- file-manager/src/main/scala/it/pagopa/interop/commons/files/service/impl/S3ManagerImpl.scala
+++ file-manager/src/main/scala/it/pagopa/interop/commons/files/service/impl/S3ManagerImpl.scala
@@ -4,16 +4,19 @@ import akka.http.scaladsl.server.directives.FileInfo
 import cats.implicits._
 import it.pagopa.interop.commons.files.StorageConfiguration
 import it.pagopa.interop.commons.files.service.FileManager
+import org.apache.commons.codec.binary.Base64
+import org.apache.commons.codec.digest.DigestUtils
 import org.slf4j.{Logger, LoggerFactory}
 import software.amazon.awssdk.core.ResponseBytes
 import software.amazon.awssdk.core.async.{AsyncRequestBody, AsyncResponseTransformer}
 import software.amazon.awssdk.core.client.config.{ClientAsyncConfiguration, SdkAdvancedAsyncClientOption}
 import software.amazon.awssdk.http.async.SdkAsyncHttpClient
 import software.amazon.awssdk.http.nio.netty.NettyNioAsyncHttpClient
-import software.amazon.awssdk.services.s3.{S3AsyncClient, S3Configuration}
 import software.amazon.awssdk.services.s3.model._
+import software.amazon.awssdk.services.s3.{S3AsyncClient, S3Configuration}
 
 import java.io.{ByteArrayOutputStream, File}
+import java.nio.file.Files
 import java.util.concurrent.Executor
 import scala.concurrent.{ExecutionContext, ExecutionContextExecutor, Future}
 import scala.jdk.FutureConverters._
@@ -46,9 +49,10 @@ final class S3ManagerImpl(blockingExecutionContext: ExecutionContextExecutor) ex
     containerPath: String,
     path: String
   )(resourceId: String, fileParts: (FileInfo, File)): Future[StorageFilePath] = {
-    val key: String                        = s3Key(path, resourceId.toString, fileParts._1.getFileName)
+    val key: String                        = s3Key(path, resourceId, fileParts._1.getFileName)
     logger.debug("Storing resource {} at path {}", resourceId, key)
-    val putObjectRequest: PutObjectRequest = PutObjectRequest.builder.bucket(containerPath).key(key).build
+    val putObjectRequest: PutObjectRequest =
+      PutObjectRequest.builder.bucket(containerPath).key(key).contentMD5(contentMd5(fileParts._2)).build
     val asyncRequestBody: AsyncRequestBody = AsyncRequestBody.fromFile(fileParts._2)
     asyncClient.putObject(putObjectRequest, asyncRequestBody).asScala.as(key)
   }
@@ -57,9 +61,10 @@ final class S3ManagerImpl(blockingExecutionContext: ExecutionContextExecutor) ex
     containerPath: String,
     path: String
   )(resourceId: String, fileName: String, fileContents: Array[Byte]): Future[StorageFilePath] = {
-    val key: String                        = s3Key(path, resourceId.toString, fileName)
+    val key: String                        = s3Key(path, resourceId, fileName)
     logger.debug("Storing resource {} at path {}", resourceId, key)
-    val putObjectRequest: PutObjectRequest = PutObjectRequest.builder.bucket(containerPath).key(key).build
+    val putObjectRequest: PutObjectRequest =
+      PutObjectRequest.builder.bucket(containerPath).key(key).contentMD5(contentMd5(fileContents)).build
     val asyncRequestBody: AsyncRequestBody = AsyncRequestBody.fromBytes(fileContents)
     asyncClient.putObject(putObjectRequest, asyncRequestBody).asScala.as(key)
   }
@@ -106,5 +111,9 @@ final class S3ManagerImpl(blockingExecutionContext: ExecutionContextExecutor) ex
       .asScala
       .map(_.deleteMarker().booleanValue())
   }
+
+  def contentMd5(file: File): String = contentMd5(Files.readAllBytes(file.toPath))
+
+  def contentMd5(byteArray: Array[Byte]): String = new String(Base64.encodeBase64(DigestUtils.md5(byteArray)))
 
 }
