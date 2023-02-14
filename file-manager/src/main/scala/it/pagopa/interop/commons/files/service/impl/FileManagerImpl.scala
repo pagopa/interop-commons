056ec90f836d63fe5aed3d870cbc78cd551306e5
Antonio Gelameris <toniogela89@gmail.com>
2023-02-14 10:41:01 +0100

    Added new s3 apis

--- file-manager/src/main/scala/it/pagopa/interop/commons/files/service/impl/FileManagerImpl.scala
+++ file-manager/src/main/scala/it/pagopa/interop/commons/files/service/impl/FileManagerImpl.scala
@@ -6,6 +6,9 @@ import it.pagopa.interop.commons.files.service.FileManager
 import java.io.{ByteArrayOutputStream, File, FileInputStream}
 import java.nio.file.{Files, Path, Paths, StandardCopyOption}
 import scala.concurrent.{ExecutionContextExecutor, Future}
+import java.nio.file.FileVisitOption
+import java.util.stream.Collectors
+import scala.jdk.CollectionConverters._
 
 final class FileManagerImpl(blockingExecutionContext: ExecutionContextExecutor) extends FileManager {
 
@@ -31,6 +34,29 @@ final class FileManagerImpl(blockingExecutionContext: ExecutionContextExecutor) 
     Files.write(destPath, fileContents).toAbsolutePath().toString()
   }
 
+  override def storeBytes(containerPath: String, path: String, filename: String)(
+    fileContents: Array[Byte]
+  ): Future[StorageFilePath] = Future(
+    Files.write(createPath(path, "", filename), fileContents).toAbsolutePath().toString()
+  )
+
+  override def listFiles(container: String)(prefix: String): Future[List[StorageFilePath]] = Future(
+    Files
+      .walk(tmp.resolve(prefix), FileVisitOption.FOLLOW_LINKS)
+      .collect(Collectors.toList[Path])
+      .asScala
+      .toList
+      .map(_.toAbsolutePath.toString)
+  )
+
+  override def getFile(container: String)(path: String): Future[Array[Byte]] = Future(
+    Files.readAllBytes(tmp.resolve(path))
+  )
+
+  override def getAllFiles(container: String)(prefix: String): Future[Map[String, Array[Byte]]] =
+    listFiles(container)(prefix)
+      .flatMap(files => Future.traverse(files)(p => getFile(container)(p).map((p, _))).map(_.toMap))
+
   override def get(containerPath: String)(filePath: String): Future[ByteArrayOutputStream] = Future {
     val outputStream: ByteArrayOutputStream = new ByteArrayOutputStream()
     new FileInputStream(filePath).transferTo(outputStream)
@@ -52,7 +78,9 @@ final class FileManagerImpl(blockingExecutionContext: ExecutionContextExecutor) 
   private def createPath(path: String, resourceId: String, fileName: String): Path = {
     val pathF: String       = path.stripMargin('/')
     val resourceIdF: String = resourceId.stripMargin('/')
-    val docsHome: Path      = tmp.resolve(s"$pathF/$resourceIdF")
+    val docsHome: Path      =
+      if (resourceIdF.isBlank()) tmp.resolve(s"$pathF")
+      else tmp.resolve(s"$pathF/$resourceIdF")
     val pathCreated: Path   = Files.createDirectories(docsHome)
     pathCreated.resolve(fileName.stripMargin('/'))
   }
