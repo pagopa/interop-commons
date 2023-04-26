ecc794be70a5ff6406126e4e0e0debe2cd2f8f1b
AsterITA <fabrizio.musella@gmail.com>
2023-04-26 14:53:11 +0200

    Added ErrorAction (#264)

--- cqrs/src/main/scala/it/pagopa/interop/commons/cqrs/service/CqrsProjectionHandler.scala
+++ cqrs/src/main/scala/it/pagopa/interop/commons/cqrs/service/CqrsProjectionHandler.scala
@@ -57,6 +57,7 @@ final case class CqrsProjectionHandler[T](eventHandler: EventHandler[T], dbName:
           } yield result
           r.toFuture()
         case MultiAction(actions)                     => actions.traverse(applyPartialAction)
+        case ErrorAction(error)                       => Future.failed(error)
         case NoOpAction                               => Future.unit
       }
 
