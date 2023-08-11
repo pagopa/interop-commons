ba6178d87c1616b887d125034aae25e248441abf
nttdata-rtorsoli <riccardo.torsoli@emeal.nttdata.com>
2023-08-11 07:56:36 +0200

    PIN-3793 Formatting

--- utils/src/main/scala/it/pagopa/interop/commons/utils/AkkaUtils.scala
+++ utils/src/main/scala/it/pagopa/interop/commons/utils/AkkaUtils.scala
@@ -89,11 +89,11 @@ trait AkkaUtils {
     getUserRoles(contexts).map(_.split(',').toList)
   )
 
-  def getExternalIdOrigin(contexts: Seq[(String, String)]): Either[Throwable, String]      =
+  def getExternalIdOrigin(contexts: Seq[(String, String)]): Either[Throwable, String] =
     fastGet(contexts)(ORGANIZATION_EXTERNAL_ID_ORIGIN_CLAIM, MissingClaim(ORGANIZATION_EXTERNAL_ID_ORIGIN_CLAIM))
-  def getExternalIdValue(contexts: Seq[(String, String)]): Either[Throwable, String]      =
-    fastGet(contexts)(ORGANIZATION_EXTERNAL_ID_VALUE_CLAIM, MissingClaim(ORGANIZATION_EXTERNAL_ID_VALUE_CLAIM))    
-  
+  def getExternalIdValue(contexts: Seq[(String, String)]): Either[Throwable, String]  =
+    fastGet(contexts)(ORGANIZATION_EXTERNAL_ID_VALUE_CLAIM, MissingClaim(ORGANIZATION_EXTERNAL_ID_VALUE_CLAIM))
+
   def getPurposeId(contexts: Seq[(String, String)]): Either[Throwable, String]   =
     fastGet(contexts)(PURPOSE_ID_CLAIM, MissingClaim(PURPOSE_ID_CLAIM))
   def getPurposeIdUUID(contexts: Seq[(String, String)]): Either[Throwable, UUID] = toFastUUID(getPurposeId(contexts))
