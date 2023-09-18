5c5c6e5cc863b7ed09ca21a4c9d7666ebb82a538
Fabrizio Musella <fabrizio.musella@pagopa.it>
2023-09-18 15:31:41 +0200

    added JWTPathInfo class

--- utils/src/main/scala/it/pagopa/interop/commons/utils/JWTPathGenerator.scala
+++ utils/src/main/scala/it/pagopa/interop/commons/utils/JWTPathGenerator.scala
@@ -6,12 +6,12 @@ import java.time.format.DateTimeFormatter
 import java.util.UUID
 
 object JWTPathGenerator {
-  def generateJWTPathInfo(dateTimeSupplier: OffsetDateTimeSupplier): (String, String) = {
+  case class JWTPathInfo(path: String, filename: String)
+
+  def generateJWTPathInfo(dateTimeSupplier: OffsetDateTimeSupplier): JWTPathInfo = {
     val now               = dateTimeSupplier.get()
     val formattedDate     = now.format(DateTimeFormatter.ofPattern("yyyyMMdd"))
     val formattedDateTime = now.format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss"))
-    val path              = s"token-details/$formattedDate"
-    val filename          = s"${formattedDateTime}_${UUID.randomUUID()}.ndjson"
-    (path, filename)
+    JWTPathInfo(s"token-details/$formattedDate", s"${formattedDateTime}_${UUID.randomUUID()}.ndjson")
   }
 }
