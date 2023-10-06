3fa57f7ce605f649aec5e63628a6ba269f9efe1d
Alessio Gallitano <25105748+galales@users.noreply.github.com>
2023-10-06 11:58:01 +0200

    fix-kms-connections-config: Set kms max acquisition timeout configuration

--- signer/src/main/scala/it/pagopa/interop/commons/signer/SignerConfiguration.scala
+++ signer/src/main/scala/it/pagopa/interop/commons/signer/SignerConfiguration.scala
@@ -3,6 +3,7 @@ package it.pagopa.interop.commons.signer
 import com.typesafe.config.{Config, ConfigFactory}
 
 import java.net.URI
+import java.time.Duration
 
 final case class VaultConfig(address: String, token: String, sslEnabled: Boolean, signatureRoute: String) {
   def encryptionEndpoint(keyId: String) = new URI(s"$address/$signatureRoute/$keyId").normalize().toString
@@ -14,6 +15,7 @@ object SignerConfiguration {
 
   private val config: Config = ConfigFactory.load()
 
-  val maxConcurrency: Int = config.getInt("interop-commons.kms.max-concurrency")
+  val maxConcurrency: Int             = config.getInt("interop-commons.kms.max-concurrency")
+  val maxAcquisitionTimeout: Duration = config.getDuration("interop-commons.kms.max-acquisition-timeout")
 
 }
