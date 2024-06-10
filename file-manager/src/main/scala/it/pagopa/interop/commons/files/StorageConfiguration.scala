6db27cf9e197958c3fe96e0eb3717742c8301dce
AsterITA <fabrizio.musella@gmail.com>
2024-06-10 10:31:59 +0200

    deleted unused import and formatting

--- file-manager/src/main/scala/it/pagopa/interop/commons/files/StorageConfiguration.scala
+++ file-manager/src/main/scala/it/pagopa/interop/commons/files/StorageConfiguration.scala
@@ -1,6 +1,6 @@
 package it.pagopa.interop.commons.files
 
-import com.typesafe.config.{Config, ConfigFactory}
+import com.typesafe.config.ConfigFactory
 
 object StorageConfiguration {
   val maxConcurrency: Int = ConfigFactory.load().getInt("interop-commons.storage.max-concurrency")
